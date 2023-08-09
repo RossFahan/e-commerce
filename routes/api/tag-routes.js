9a865b9d63a6cf27f9d261bed28d3f9cd47aa385
Ross F <rfahan@gmail.com>
2023-08-08 19:21:18 -0600

    completed category routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,7 +35,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const tag = await Tag.create(req.body);
-    res.status(201).json(tag);
+    res.status(200).json(tag);
   } catch (err) {
     res.status(400).json(err);
   }
