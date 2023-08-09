d04e94699b602d2ba3bb30057c64a800d5a02cc7
Ross F <rfahan@gmail.com>
2023-08-08 18:47:40 -0600

    added getters to tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,32 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// GET all tags
+router.get('/', async (req, res) => {
+  try {
+    const tags = await Tag.findAll({
+      include: [{ model: Product, through: ProductTag }],
+    });
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+// GET one tag by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag }],
+    });
+    if (!tag) {
+      res.status(404).json({ message: 'No tag found with this ID' });
+      return;
+    }
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
